e2cc18f3b26f5cafeb85e54b0cf533cf3d7c0a80
Franki彩虹 <caihongfran@yeah.net>
2021-05-05 10:09:14 +0800

    code

---
+++ 上位机C#/GUI.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using ArduinoAudioPlayer;
+
+using Un4seen.Bass;
+
+namespace ArduinoAudioPlayer
+{
+    public class GUI
+    {
+        private Canvas root;
+        public int width = 500;
+        public int height = 300;
+        public void GUI_Init(Canvas canvas)
+        {
+            root = canvas;
+        }
+        public void FillCircle(double x, double y, int r, Brush Color)
+        {
+            if (r < 0)
+            {
+                return;
+            }
+            Path myPath = new Path();
+            myPath.Fill = Color;
+            myPath.HorizontalAlignment = HorizontalAlignment.Left;
+            myPath.VerticalAlignment = VerticalAlignment.Center;
+            EllipseGeometry myEllipseGeometry = new EllipseGeometry();
+            myEllipseGeometry.Center = new System.Windows.Point(x, y);
+            myEllipseGeometry.RadiusX = r;
+            myEllipseGeometry.RadiusY = r;
+            myPath.Data = myEllipseGeometry;
+
+            root.Children.Add(myPath);
+
+        }
+        
+    
+        public void DrawingLine(double x, double y, double x1, double y1,double thickness, Brush color)
+        {
+            Point startPt = new Point(x, y);
+            Point endPt = new Point(x1, y1);
+            LineGeometry myLineGeometry = new LineGeometry();
+            myLineGeometry.StartPoint = startPt;
+            myLineGeometry.EndPoint = endPt;
+            Path LinePath = new Path();
+            LinePath.Stroke = color;
+            LinePath.StrokeThickness = thickness;
+            LinePath.Data = myLineGeometry;
+
+            root.Children.Add(LinePath);
+
+        }
+     
+
+      
+        public void Rec(double x, double y, double w, Brush color)
+        {
+            RectangleGeometry myRectangleGeometry = new RectangleGeometry();
+            myRectangleGeometry.Rect = new Rect(x, height - y, w, y);
+            Path myPath = new Path();
+            myPath.Data = myRectangleGeometry;
+            myPath.Fill = color;
+            myPath.StrokeThickness = 0;
+            root.Children.Add(myPath);
+        }
+        public void Fill_Rect(double x, double y, double w,double h, Brush color)
+        {
+            RectangleGeometry myRectangleGeometry = new RectangleGeometry();
+            myRectangleGeometry.Rect = new Rect(x, y, w, h);
+            Path blockTop = new Path();
+            blockTop.Fill = color;
+            blockTop.StrokeThickness = 0;
+            blockTop.Data = myRectangleGeometry;
+            root.Children.Add(blockTop);
+        }
+        public void Circle(double x, double y, double r, Brush Color)
+        {
+            if (r < 0)
+            {
+                return;
+            }
+            Path myPath = new Path();
+            myPath.Stroke = Color;
+            myPath.HorizontalAlignment = HorizontalAlignment.Left;
+            myPath.VerticalAlignment = VerticalAlignment.Center;
+            EllipseGeometry myEllipseGeometry = new EllipseGeometry();
+            myEllipseGeometry.Center = new System.Windows.Point(x, y);
+            myEllipseGeometry.RadiusX = r;
+            myEllipseGeometry.RadiusY = r;
+            myPath.Data = myEllipseGeometry;
+
+            root.Children.Add(myPath);
+        }
+    }
+}
---
+++ 上位机C#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Un4seen.Bass;
+using Un4seen.Bass.AddOn.Mix;
+using Un4seen.BassWasapi;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+using System.Configuration;
+using System.Windows;
+using Upper_computer;
+namespace ArduinoAudioPlayer
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            BassNet.Registration(ConfigurationManager.AppSettings["bass_email"], ConfigurationManager.AppSettings["bass_key"]);
+            Tool tool = new Tool();
+            //OLEDwin win = new OLEDwin();
+            MainWindow main = new MainWindow();
+            Application app = new Application();
+            /*串口初始化*/
+            app.Run(main);
+            Bass.BASS_Free();
+            
+        }
+
+    }
+}
